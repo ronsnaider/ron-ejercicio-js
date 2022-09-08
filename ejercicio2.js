2b9a231d7c02c708da67b501250ef97338334dd8
ronsnaider <109690769+ronsnaider@users.noreply.github.com>
2022-09-08 17:27:00 +0200

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+const nombre = "juan Carlos";
+const edad = 45;
+const desarrollador = false;
+const fecha = new Date (1977, 11 ,18);
+const libro = {
+    titulo: "brew",
+    autor: "james morton",
+    fecha: 2016,
+    url:"www.librooks.es",
+
+}
+console.log(nombre, edad, desarrollador, libro);
+console.log(fecha);
+
